a164b50762ba8c66d9aadb5db3c76c535e7e71e6
Jan Pecha <janpecha@email.cz>
2026-01-09 09:42:31 +0100

    Added declare(strict_types=1)

--- src/ComposerCleaner/Cleaner.php
+++ src/ComposerCleaner/Cleaner.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * Victor The Cleaner for Composer.
  *
--- tests/mocks.php
+++ tests/mocks.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Composer\IO {
 
 	interface IOInterface
